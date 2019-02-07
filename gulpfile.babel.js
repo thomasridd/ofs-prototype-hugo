445a1cf249f834c809927ce4a546960d02ac2374
Tom Ridd <twridd@gmail.com>
2019-02-07 15:48:12 +0000

    change gulp port

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,38 +13,44 @@ import inject from "gulp-inject";
 import cssnano from "cssnano";
 
 const browserSync = BrowserSync.create();
-const hugoBin = `./bin/hugo.${process.platform === "win32" ? "exe" : process.platform}`;
+const hugoBin = `./bin/hugo.${
+  process.platform === "win32" ? "exe" : process.platform
+}`;
 const defaultArgs = ["-d", "../dist", "-s", "site"];
 
 if (process.env.DEBUG) {
-  defaultArgs.unshift("--debug")
+  defaultArgs.unshift("--debug");
 }
 
-gulp.task("hugo", (cb) => buildSite(cb));
-gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
+gulp.task("hugo", cb => buildSite(cb));
+gulp.task("hugo-preview", cb =>
+  buildSite(cb, ["--buildDrafts", "--buildFuture"])
+);
 gulp.task("build", ["css", "js", "hugo"]);
 gulp.task("build-preview", ["css", "js", "hugo-preview"]);
 
-gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
-    .pipe(postcss([
-      cssImport({from: "./src/css/main.css"}),
-      cssnext(),
-      cssnano(),
-    ]))
+gulp.task("css", () =>
+  gulp
+    .src("./src/css/*.css")
+    .pipe(
+      postcss([cssImport({ from: "./src/css/main.css" }), cssnext(), cssnano()])
+    )
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
-));
+);
 
-gulp.task("js", (cb) => {
+gulp.task("js", cb => {
   const myConfig = Object.assign({}, webpackConfig);
 
   webpack(myConfig, (err, stats) => {
     if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
+    gutil.log(
+      "[webpack]",
+      stats.toString({
+        colors: true,
+        progress: true
+      })
+    );
     browserSync.reload();
     cb();
   });
@@ -54,7 +60,7 @@ gulp.task("svg", () => {
   const svgs = gulp
     .src("site/static/img/icons-*.svg")
     .pipe(svgmin())
-    .pipe(svgstore({inlineSvg: true}));
+    .pipe(svgstore({ inlineSvg: true }));
 
   function fileContents(filePath, file) {
     return file.contents.toString();
@@ -62,7 +68,7 @@ gulp.task("svg", () => {
 
   return gulp
     .src("site/layouts/partials/svg.html")
-    .pipe(inject(svgs, {transform: fileContents}))
+    .pipe(inject(svgs, { transform: fileContents }))
     .pipe(gulp.dest("site/layouts/partials/"));
 });
 
@@ -70,7 +76,8 @@ gulp.task("server", ["hugo", "css", "js", "svg"], () => {
   browserSync.init({
     server: {
       baseDir: "./dist"
-    }
+    },
+    port: process.env.PORT || 3001
   });
   gulp.watch("./src/js/**/*.js", ["js"]);
   gulp.watch("./src/css/**/*.css", ["css"]);
@@ -81,7 +88,7 @@ gulp.task("server", ["hugo", "css", "js", "svg"], () => {
 function buildSite(cb, options) {
   const args = options ? defaultArgs.concat(options) : defaultArgs;
 
-  return cp.spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
+  return cp.spawn(hugoBin, args, { stdio: "inherit" }).on("close", code => {
     if (code === 0) {
       browserSync.reload("notify:false");
       cb();
